d509cc475bf44424b995585517d1fbd30558e116
rocnail23 <jdanielrojas16@gmail.com>
2023-11-12 05:48:12 -0400

    improve slider

--- src/Ecommerce/components/Slider.tsx
+++ src/Ecommerce/components/Slider.tsx
@@ -1,95 +1,79 @@
-import { ImageCarrou as data } from "../data"
-import  { useEffect, useRef, useState, } from 'react'
+import { ImageCarrou as data } from "../data";
+import { useEffect, useRef, useState } from 'react';
 import { SliderImg } from './SliderImg';
-import "../styles/Slider.css"
-
+import "../styles/Slider.css";
 
 export const Slider = () => {
-
-  const [changing, setChangin] = useState(false)
+  const [changing, setChanging] = useState(false);
   const [currentIndex, setCurrentIndex] = useState(0);
-  const [showImage, setShowImage] = useState(false)
-  const interval = useRef<ReturnType<typeof setInterval>>()
-  const timeOut = useRef<ReturnType<typeof setInterval>>()
-  
+  const [showImage, setShowImage] = useState(false);
+
+  const interval = useRef<ReturnType<typeof setInterval> | null>(null);
+  const timeout = useRef<ReturnType<typeof setInterval> | null>(null);
+
   useEffect(() => {
-   
-   clearInterval(interval.current) 
-   clearTimeout(timeOut.current) 
-   interval.current = setInterval(() => {
-      setShowImage(false)
+    clearInterval(interval.current!);
+    clearTimeout(timeout.current!);
+
+    interval.current = setInterval(() => {
+      setShowImage(false);
       const isLastSlide = currentIndex === data.length - 1;
 
-     timeOut.current = setTimeout(() => {
-        if (!isLastSlide) { 
-          setCurrentIndex(curr => curr + 1);
-       
-        }else{
-          setCurrentIndex(0)
-          
-        }
-        
-      },500)
-      
-    },5000)
+      timeout.current = setTimeout(() => {
+        setCurrentIndex(curr => (isLastSlide ? 0 : curr + 1));
+      }, 500);
+    }, 5000);
 
-    
-
-   
-  },[currentIndex])
+    return () => {
+      clearInterval(interval.current!);
+      clearTimeout(timeout.current!);
+    };
+  }, [currentIndex]);
 
   const changeShowImage = () => {
-    setShowImage(true)
-  }
-  
-  const scrollToImage = (direction:string) => {
-    if(changing) return
-    setChangin(true)
-    setShowImage(false)
-   setTimeout(() => {
-    if (direction === 'prev') {
+    setShowImage(true);
+  };
+
+  const scrollToImage = (direction: 'prev' | 'next') => {
+    if (changing) return;
+
+    setChanging(true);
+    setShowImage(false);
+
+    setTimeout(() => {
       setCurrentIndex(curr => {
         const isFirstSlide = currentIndex === 0;
-        return isFirstSlide ? 0 : curr - 1;
-      })
-    } else {
-      const isLastSlide = currentIndex === data.length - 1;
-      if (!isLastSlide) {
-        setCurrentIndex(curr => curr + 1);
-      }else{
-        setCurrentIndex(0)
-      }
-    }
-    setChangin(false)
-   },500)
-   
-  }
+        return direction === 'prev' ? (isFirstSlide ? data.length - 1 : curr - 1) : (curr + 1) % data.length;
+      });
 
-  
+      setChanging(false);
+    }, 500);
+  };
 
-  const goToSlide = (slideIndex:number) => {
+  const goToSlide = (slideIndex: number) => {
     setCurrentIndex(slideIndex);
-  }
-  
+  };
+
   return (
     <div className="main-container">
       <div className="slider-container">
         <div className='btn-leftArrow' onClick={() => scrollToImage('prev')}>&#10092;</div>
         <div className='btn-rightArrow' onClick={() => scrollToImage('next')}>&#10093;</div>
-        <div className={`container-images ${showImage && "showImage"}`}>     
-                 <SliderImg img={data[currentIndex]?.img} changeShowImage={changeShowImage}/>
+        <div className={`container-images ${showImage && "showImage"}`}>
+          <SliderImg img={data[currentIndex]?.img} changeShowImage={changeShowImage} />
         </div>
         <div className="dots-container">
-          {
-            data.map((_, idx) => (
-              <div key={idx}
-                className={`dot-container-item ${idx === currentIndex ? "active" : ""}`}
-                onClick={() => goToSlide(idx)}>
-                &#9865;
-              </div>))
-          }
+          {data.map((_, idx) => (
+            <div
+              key={idx}
+              className={`dot-container-item ${idx === currentIndex ? "active" : ""}`}
+              onClick={() => goToSlide(idx)}
+            >
+              &#9865;
+            </div>
+          ))}
         </div>
       </div>
-    </div >
-  )
-}
+    </div>
+  );
+};
